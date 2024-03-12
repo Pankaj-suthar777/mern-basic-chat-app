cd3df1af37b7cb1d52eecbdb28f1985894cc8b7d
pankaj suthar <pankajss0070@gmail.com>
2024-03-12 12:44:31 +0530

    login signup logout

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectDb.js";
 
 const app = express();
@@ -10,10 +13,12 @@ const PORT = 5000;
 
 dotenv.config();
 
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
